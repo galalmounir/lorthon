3dcc66b12e63839b9a30c20751d9708928b89e86
Timon Tscheulin <timon.tscheulin@gmail.com>
2020-04-22 09:09:38 +0200

    Clean code

--- lorthon.pyx
+++ lorthon.pyx
@@ -1,25 +1,28 @@
 cdef extern from "interface_lora.h":
-	void LoRaTx( unsigned int freq, char tx_mode,
-			char rf_chain, char rf_power, 
-			char bandwidth, unsigned int datarate, char coderate,
-			unsigned short preamble, char no_crc,
-			char no_header, unsigned char* payload, unsigned short size);
-	char LoRaInit(const char * confFile);
-	void LoRaStop();
-	char* LoRaRx();
+     void LoRaTx( unsigned int freq, char tx_mode,
+                  char rf_chain, char rf_power, 
+                  char bandwidth, unsigned int datarate, char coderate,
+                  unsigned short preamble, char no_crc,
+                  char no_header, unsigned char* payload, unsigned short size);
+     char LoRaInit(const char * confFile);
+     void LoRaStop();
+     char* LoRaRx();
 
 def py_LoRaTx(freq: int, tx_mode: int, rf_chain: int, rf_power: int, bandwidth: int, datarate: int, 
 		coderate: int, preamble: int, no_crc: bool, no_header: bool, payload: bytes, size: int) -> None:
+    LoRaTx(freq, tx_mode, rf_chain, rf_power, bandwidth, datarate, coderate, preamble, no_crc, no_header, payload, size)
 
-	LoRaTx(freq, tx_mode, rf_chain, rf_power, bandwidth, datarate, coderate, preamble, no_crc, no_header, payload, size)
 def py_LoRaInit(confFile):
-	value = LoRaInit (confFile)
-	return value
+    confFile = bytes(confFile, 'ascii')
+    value = LoRaInit (confFile)
+    return value
+
 def py_LoRaStop():
-	LoRaStop()
+    LoRaStop()
+
 def py_LoRaRx():
-	value = LoRaRx()
-	return value
+    value = LoRaRx()
+    return value
 
 
 
--- tst/test.py
+++ tst/test.py
@@ -1,14 +1,18 @@
 import lorthon
 import time
 
-if (lorthon.py_LoRaInit("global_conf.json") == 0):
-	while True:
-		value = lorthon.py_LoRaRx()
-		print value
-		time.sleep(1)
-		lorthon.py_LoRaTx(915000000, 0, 0, 22, 0x03, 0x02, 1, 15, False, False, "HELLO WORLD", 11)
-		time.sleep(5)
-else:
-	print "ERROR!!!!"
-	
-lorthon.py_LoRaStop();	
+
+if __name__ == "__main__":
+    initRet = lorthon.py_LoRaInit("global_conf.json")
+    if(initRet == 0):
+        print("Lorthon init successful!")
+        #while True:
+            #value = lorthon.py_LoRaRx()
+            #print(value)
+            #time.sleep(1)
+            #lorthon.py_LoRaTx(915000000, 0, 0, 22, 0x03, 0x02, 1, 15, False, False, "HELLO WORLD", 11)
+            #time.sleep(5)
+    else:
+        print(f"Lorthon init Error: {initRet}")
+
+    lorthon.py_LoRaStop()
